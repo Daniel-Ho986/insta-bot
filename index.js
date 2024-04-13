f5c7a7bc626a63474a8d25f681ce43fb31a87b37
Daniel <dhonaruto@gmail.com>
2024-04-12 20:45:44 -0400

    instagram posting issue

--- index.js
+++ index.js
@@ -1,143 +1,76 @@
-// Blog: https://www.ryancarmody.dev/blog/how-to-automate-instagram-posts-with-nodejs
-
-// Tutorial: https://www.youtube.com/watch?v=XzyYi_yv86A&ab_channel=AviMamenko
-
-// Hosting Platform: https://www.youtube.com/watch?v=MusIvEKjqsc&t=50s&ab_channel=TraversyMedia
-
-const express = require("express");
-const app = express();
-
-const axios = require("axios");
-const Instagram = require("./utils/instagram-web-api");
-const { generateAndSaveImage } = require("./draw-image");
-
 require("dotenv").config();
+const express = require("express");
+const { CronJob } = require("cron");
+const { IgApiClient } = require('instagram-private-api');
+const { generateAndSaveImage } = require('./draw-image');
+const { readFile } = require('fs');
+const { promisify } = require('util');
+const readFileAsync = promisify(readFile);
 
+const app = express();
 const port = process.env.PORT || 4000;
 
-// const { CronJob } = require("cron");
-// const fs = require("fs");
-
-const instagramLoginFunction = () => {
-  const client = new Instagram({
-    username: process.env.INSTAGRAM_USERNAME,
-    password: process.env.INSTAGRAM_PASSWORD,
-  });
-
-  const instagramPostPictureFunction = async () => {
-    await client
-      .getPhotosByUsername({ username: process.env.INSTAGRAM_USERNAME })
-      .then(
-        (res) =>
-          res.user.edge_owner_to_timeline_media.edges.map(
-            (edge) => edge.node.edge_media_to_caption.edges[0].node.text
-          )[0]
-      )
-      .then((mostRecent) => Number(mostRecent.split(" - ")[0]))
-      .then((latestNumber) => {
-        const updatedNumber = latestNumber + 1;
-
-        const inkyDoodleQuery = `
-        query {
-          inkyDoodleCollection(where: { number: ${updateNumber}}) {
-            items {
-              number
-              generation
-              name
-              parents
-              image {
-                url
-              }
-            }
-          }
-        }
-        `;
-
-        axios({
-          url: `https://graphql.contentful.com/content/v1/space/${process.env.CONTENTFUL_SPACE_ID}`,
-          method: "post",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
-          },
-          data: {
-            query: inkyDoodleQuery,
-          },
-        })
-          .then((res) => res.data)
-          .then(({ data, errors }) => {
-            if (errors) {
-              console.log(errors);
-            }
-
-            const updatedInkyDoodle = data.inkyDoodleCollection.items[0];
-            if (updatedInkyDoodle) {
-              const updateCaption = `${updatedNumber} - ${
-                updatedInkyDoodle.name
-              }\n${
-                updatedInkyDoodle.parents
-                  ? updatedInkyDoodle.parents.length > 0
-                    ? updatedInkyDoodle.parents
-                        .map((parent) => "#" + parent)
-                        .join(" + ") + " \n"
-                    : ""
-                  : ""
-              }#inkydoodle #gen${updatedInkyDoodle.generation}`;
-            }
-          });
-      });
-  };
-  instagramPostPictureFunction();
-};
-
-instagramLoginFunction();
-
 app.listen(port, () => {
-  console.log(`Listening to port ${port}...`);
+    console.log(`Listening to port ${port}...`);
 });
 
-async function main() {
-  try {
-    // Fetch quote and image URL
-    const quoteData = await getZenQuote();
-    const imageUrl = await fetchRandomImageURL();
+const postToInsta = async () => {
+    const ig = new IgApiClient(); 
+    ig.state.generateDevice(process.env.INSTAGRAM_USERNAME);
+    await ig.account.login(process.env.INSTAGRAM_USERNAME, process.env.IG_PASSWORD);
 
-    // Generate and save image
-    const outputPath = "./generated-image.jpeg";
-    await generateAndSaveImage(quoteData, imageUrl, outputPath);
+    const path = './generated-image.jpeg'
 
-    // Post image to Instagram
-    // await postImageToInstagram(outputPath);
-  } catch (error) {
-    console.error("Error:", error);
-  }
+    // await ig.publish.photo({
+    //     file: path,
+    //     caption: 'Really nice photo!',
+    // });
 }
 
-// main();
+postToInsta();
+
+// async function main() {
+//     try {
+//         // Fetch quote and image URL
+//         const quoteData = await getZenQuote();
+//         const imageUrl = await fetchRandomImageURL();
+
+//         // Generate and save image
+//         const outputPath = './generated-image.jpeg';
+//         await generateAndSaveImage(quoteData, imageUrl, outputPath);
+
+//         // Post image to Instagram
+//         // await postImageToInstagram(outputPath);
+//     } catch (error) {
+//         console.error('Error:', error);
+//     }
+// }
+
+// main()
 // Define cron job to run the main function every day at 5:30 AM
 // const cronJob = new CronJob("30 5 * * *", main);
 // cronJob.start();
 
 // Function to fetch today's quote from ZenQuotes API
-async function getZenQuote() {
-  const api_url = "https://zenquotes.io/api/today/";
-  const response = await fetch(api_url);
-  const data = await response.json();
-  return data[0];
-}
+// async function getZenQuote() {
+//     const api_url = "https://zenquotes.io/api/today/";
+//     const response = await fetch(api_url);
+//     const data = await response.json();
+//     return data[0];
+// }
 
 // Function to fetch a random nature image from Pexels API
-async function fetchRandomImageURL() {
-  const url = `https://api.pexels.com/v1/search?query=nature&per_page=30`;
-  const response = await fetch(url, {
-    headers: {
-      Authorization: process.env.PEXELS_API_KEY,
-    },
-  });
-  const data = await response.json();
-  if (data && data.photos && data.photos.length > 0) {
-    const randomIndex = Math.floor(Math.random() * data.photos.length);
-    return data.photos[randomIndex].src.original;
-  }
-  return null;
-}
+// async function fetchRandomImageURL() {
+//     const url = `https://api.pexels.com/v1/search?query=nature&per_page=30`;
+//     const response = await fetch(url, {
+//         headers: {
+//             Authorization: process.env.PEXELS_API_KEY,
+//         },
+//     });
+//     const data = await response.json();
+//     if (data && data.photos && data.photos.length > 0) {
+//         const randomIndex = Math.floor(Math.random() * data.photos.length);
+//         return data.photos[randomIndex].src.original;
+//     }
+//     return null;
+// }
